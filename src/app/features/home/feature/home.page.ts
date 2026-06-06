b72b875e285adf3ee6351d5e635b57a3f51974f6
R4r4s <rngheru@gmail.com>
2026-06-06 21:09:33 +0200

    feat(home): Refine hero CTA buttons

--- src/app/features/home/feature/home.page.ts
+++ src/app/features/home/feature/home.page.ts
@@ -1,11 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { NgOptimizedImage } from '@angular/common';
+import { LucideArrowRight, LucideCalendar } from '@lucide/angular';
 
 import { TranslatePipe } from '@shared/pipes/translate.pipe';
 
 @Component({
   selector: 'fv-home-page',
-  imports: [NgOptimizedImage, TranslatePipe],
+  imports: [NgOptimizedImage, LucideArrowRight, LucideCalendar, TranslatePipe],
   templateUrl: './home.page.html',
   styleUrl: './home.page.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
