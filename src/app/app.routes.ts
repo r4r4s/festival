fde8431ce700abf192d7aa486a570c548f1a7833
R4r4s <rngheru@gmail.com>
2026-06-07 17:58:19 +0200

    feat(routing): Add /mapa lazy route for festival map

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,4 +6,11 @@ export const routes: Routes = [
     loadChildren: () =>
       import('@features/home/home.routes').then((m) => m.HOME_ROUTES),
   },
+  {
+    path: 'mapa',
+    loadChildren: () =>
+      import('@features/festivales-map/festivales-map.routes').then(
+        (m) => m.FESTIVALES_MAP_ROUTES,
+      ),
+  },
 ];
